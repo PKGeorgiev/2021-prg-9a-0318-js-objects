d490de107dfff16a36397c1d8febcb8830058520
Ïvelin Vasilev <igvasilev19@codingburgas.bg>
2021-03-18 11:06:35 +0200

    Add Lab1.js

---
+++ IGVasilev19/Lab3.js
@@ -0,0 +1,2 @@
+let showMessageProc=(message= "No value defined") => console.log(message);
+showMessageProc("Hello function expressions and arrow function!");
---
+++ IGVasilev19/Lab4.js
@@ -0,0 +1,6 @@
+let showMessageProc=(message= "No value defined") => 
+{
+    console.log(message+" plus function body");
+    console.log("1" + 1);
+}
+showMessageProc("Hello function expressions and arrow function!");
