c9c27b410c760bfbfe2091699595d1bf2da3e5ab
Добрин Ивелинов Пейчев <DIPeychev19@codingburgas.bg>
2022-10-20 12:59:50 +0300

    Add example for threads

---
+++ ThreadSltCpp/ThreadSltCpp/ThreadInCpp.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+#include <thread>
+#include <string>
+#include <chrono>
+using namespace std;
+
+void log(string msg)
+{
+    //this_thread::sleep_for(chrono::milliseconds(1000));
+
+    auto tid = this_thread::get_id();
+
+    cout << tid << setw(5) << ": " << msg << "\n";
+}
+
+void displayPlus()
+{
+    log("displayPlus starts");
+
+    for (size_t i = 0; i < 500; i++)
+    {
+        cout << "+";
+    }
+
+    log("displayPlus ends");
+}
+
+void displayMinus()
+{
+    log("displayMinus starts");
+
+    for (size_t i = 0; i < 500; i++)
+    {
+        cout << "-";
+    }
+
+    log("displayMinus starts");
+}
+
+int main()
+{
+    log("Main thread starts");
+
+    //Create and start thread 1 and 2
+    thread t1(displayPlus);
+    thread t2(displayMinus);
+
+    log("Main thread waits");
+
+
+    //Blocks the current thread (MT)
+    //until t1 finishes execution
+    t1.join();
+    t2.join();
+
+    log("Main thread ends");
+}
